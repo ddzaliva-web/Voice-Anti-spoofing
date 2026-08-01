9ff26811cff0f79762a2c247b6d6ba1b465fcb52
ddzaliva-web <dd_zaliva@mail.ru>
2026-08-01 20:45:54 +0300

    987654321

--- src/datasets/example.py
+++ src/datasets/example.py
@@ -50,7 +50,7 @@ class  ASVSpoofDataset(BaseDataset):
             for line in f:
                 audio_line = line.strip().split()
                 audio_name = audio_line[1]
-                label = audio_line[4]
+                label = audio_line[-1]
                 if label == 'bonafide':
                     label = 1
                 else:
